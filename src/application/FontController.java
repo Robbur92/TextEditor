7e7a96af599f729d865d181de46202c82338d3a8
Robin <robbur-@hotmail.com>
2018-11-29 03:24:19 +0100

    Second commit: added functionality to rediger and fil also added some function to the font window

--- src/application/FontController.java
+++ src/application/FontController.java
@@ -2,14 +2,86 @@ package application;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class FontController implements Initializable {
+	
+	@FXML
+	private Button okButton;
+	@FXML
+	private Button cancelButton;
+	@FXML
+	private ListView<String> fontListView;
+	@FXML
+	private ListView<String> styleListView;
+	@FXML
+	private ListView<Integer> sizeListView;
+	@FXML
+	private TextField chosenFont;
+	@FXML
+	private TextField chosenStyle;
+	@FXML
+	private TextField chosenSize;
+	@FXML
+	private Label textExample;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
 		
+		fontListView.getItems().clear();
+		fontListView.getItems().addAll(
+				"Helvetica",
+				"Baskerville",
+				"Times",
+				"Arial",
+				"Gotham"
+				);
+		styleListView.getItems().clear();
+		styleListView.getItems().addAll(
+				"Vanlig",
+				"Kursiv",
+				"Fet",
+				"Fet kursiv"
+				);
+		
+		sizeListView.getItems().clear();
+		sizeListView.getItems().addAll(
+				8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72
+				);
+		fontListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				chosenFont.clear();
+				chosenFont.appendText(fontListView.getSelectionModel().getSelectedItem());
+			}
+		});
+		styleListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				chosenStyle.clear();
+				chosenStyle.appendText(styleListView.getSelectionModel().getSelectedItem());
+			}
+		});
+		sizeListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				chosenSize.clear();
+				chosenSize.appendText(sizeListView.getSelectionModel().getSelectedItem().toString());
+			}
+		});
+	}
+	
+	public void CloseFontWindow(ActionEvent event) {
+		Stage s = (Stage) cancelButton.getScene().getWindow();
+		s.close();	
 	}
 }
--- src/application/MainController.java
+++ src/application/MainController.java
@@ -8,38 +8,102 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.UnaryOperator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.JOptionPane;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.control.TextFormatter.Change;
 import javafx.stage.FileChooser;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import javafx.stage.FileChooser.ExtensionFilter;
 
 public class MainController implements Initializable {
 	
 	@FXML
+	private Menu fileMenu;
+	@FXML
 	private TextArea textArea;
+	@FXML
+	private MenuItem newFile;
+	@FXML
+	private MenuItem openFile;
+	@FXML
+	private MenuItem saveFile;
+	@FXML
+	private MenuItem saveFileAs;
+	@FXML
+	private MenuItem closeEditor;
+	
+	@FXML
+	private Menu editMenu;
+	@FXML
+	private MenuItem undoText;
+	@FXML
+	private MenuItem cutText;
+	@FXML
+	private MenuItem copyText;
+	@FXML
+	private MenuItem pasteText;
+	@FXML
+	private MenuItem deleteText;
+	@FXML
+	private MenuItem markAllText;
+	@FXML
+	private MenuBar menuBar;
+	
+	@FXML
+	private Menu formatMenu;
+	@FXML
+	private MenuItem textFont;
+	
+	@FXML
+	private Menu helpMenu;
+	@FXML
+	private MenuItem showHelp;
+	@FXML 
+	private MenuItem aboutEditor;
+	
+	private File openedFile;
+	
+	String copiedText = "";
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
-		
-		
 	}
 	
+	
+	public void CreateNewFile(ActionEvent event) {
+		
+		textArea.clear();
+		/*try {
+			File file = new File(JOptionPane.showInputDialog("Name of textfile: ") + ".txt");
+			
+			if(!file.exists()) {
+				file.createNewFile();
+			}	
+			
+			
+		}
+		catch(IOException ex) {
+			Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+		}	*/
+	}
 	//Function for opening a file and appending the contents to the textArea
-	public void SelectOpen(ActionEvent event) {
+	public void OpenFile(ActionEvent event) {
 		
 		FileChooser fc = new FileChooser();
 		
@@ -51,6 +115,7 @@ public class MainController implements Initializable {
 				new ExtensionFilter("TXT files", "*.txt"));
 		//Variable for the chosen text file
 		File selectedFile = fc.showOpenDialog(null);
+		openedFile = selectedFile;
 		
 		try {			
 			//The chosen file is run through the readFile function and the text is added to the textarea
@@ -63,7 +128,7 @@ public class MainController implements Initializable {
 		}
 	}
 	
-	public void SelectSaveAs(ActionEvent event) {
+	public void SaveFileAs(ActionEvent event) {
 		FileChooser fc = new FileChooser();
 		
 		fc.getExtensionFilters().addAll(
@@ -71,6 +136,11 @@ public class MainController implements Initializable {
 		File file = fc.showSaveDialog(null);
 		
 		if(file != null) {
+			UnaryOperator<Change> filter = c -> {
+			    c.setText(c.getText().replaceAll("\r", "\n"));
+			    return c ;
+			};
+			textArea.setTextFormatter(new TextFormatter<>(filter));
 			SaveFile(textArea.getText(), file);
 		}
 	}
@@ -106,7 +176,7 @@ public class MainController implements Initializable {
         return stringBuffer.toString();
     }
 	
-	//Saves the content of the textArea to a text file
+	//Saves the content of the textArea to a new text file
 	public void SaveFile(String text, File file) {
 		
 		try {
@@ -114,6 +184,29 @@ public class MainController implements Initializable {
 			fw = new FileWriter(file);
 			fw.write(text);
 			fw.close();
+		}
+		catch(IOException ex) {
+			Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+	
+	public void SaveToCurrentFile(ActionEvent event) {
+		try {
+			UnaryOperator<Change> filter = c -> {
+			    c.setText(c.getText().replaceAll("\r", "\n"));
+			    return c ;
+			};
+			textArea.setTextFormatter(new TextFormatter<>(filter));
+			FileWriter fw;
+			if(openedFile == null) {
+				JOptionPane.showMessageDialog(null, "Ingen fil er valgt, lagre som ny fil!","Alert",JOptionPane.WARNING_MESSAGE);
+				return;
+			}else {
+				fw = new FileWriter(openedFile);
+				fw.write(textArea.getText());
+				fw.close();
+			}
+			
 		}
 		catch(IOException ex) {
 			Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
@@ -133,6 +226,31 @@ public class MainController implements Initializable {
 		catch(Exception e) {
 	           JOptionPane.showMessageDialog(null, "Error opening font window", "Font window error", 0);
 	    }
+	}
+	
+	public void DeleteSelected(ActionEvent event) {
+		textArea.setText(textArea.getText().replace(textArea.getSelectedText(), ""));
+	}
+	
+	public void CopySelectedText(ActionEvent event) {
+		copiedText = textArea.getSelectedText();
+	}
+	
+	public void PasteCopiedText(ActionEvent event) {
+		textArea.appendText("" + copiedText);
+	}
+	
+	public void CutSelectedText(ActionEvent event) {
+		copiedText = textArea.getSelectedText();
+		textArea.setText(textArea.getText().replace(textArea.getSelectedText(), ""));
+	}
+	
+	public void SelectAllText(ActionEvent event) {
+		textArea.selectAll();
+	}
+	
+	public void CloseWindow(ActionEvent event) {
+		Platform.exit();
 	}
 }
 
